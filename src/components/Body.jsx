0878bd4d982183d1f99adf2ab165e4a6323d4d64
Happy Samal <162830866+Happy-Samal@users.noreply.github.com>
2024-08-02 16:02:06 +0530

    Update Body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -55,12 +55,6 @@ function Body() {
         toast('🦄 Password deleted', {
                 position: "top-right",
                 autoClose: 1000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
             });
     }
     const deleteClick = (id) => {
